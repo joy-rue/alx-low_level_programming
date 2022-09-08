4d37c7d8e5091218f60dee15d3eb193d0975ee30
joy-rue <ruejoysithole@gmail.com>
2022-09-08 18:57:23 +0000

    First betty check pass

--- 0x00-hello_world/4-put.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/*Function that prints text to the console */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - the main function
+ *
+ *Description: Thats all it does basically
+ *section header: prints a message to console
+ *Return: the result integer which is always zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
