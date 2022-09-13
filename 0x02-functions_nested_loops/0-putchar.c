62494e79e790503f02e63a692cce9e05a442cc60
joy-rue <ruejoysithole@gmail.com>
2022-09-13 20:11:09 +0000

    print and add a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main  - print word
+ *
+ * Description: Longer description of the function)?
+ * Return: Always zero
+ **/
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+
+}
