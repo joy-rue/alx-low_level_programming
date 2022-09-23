df6b471b846b71942c9bb5563d9e61636969e9d7
joy-rue <ruejoysithole@gmail.com>
2022-09-23 01:16:02 +0000

    print the lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,15 @@
 /**
  * main - prints lowercase letters in the alphabet using putchar
  *
- * return: just zero
+ * return - just 0
  */
 
 int main(void)
 {
 
 	char letter;
-	for (letter = 'a'; letter <= 'z'; ++letter)
 
+	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		putchar(letter);
 	}
