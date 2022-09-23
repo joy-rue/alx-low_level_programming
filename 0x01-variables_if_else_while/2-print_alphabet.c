35e3ec216b524fb6bdfaa4dde0042925dd5453b4
joy-rue <ruejoysithole@gmail.com>
2022-09-23 01:02:34 +0000

    print the lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase letters in the alphabet using putchar
+ *
+ * return: just zero
+ */
+
+int main(void)
+{
+
+	char letter;
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+
+		putchar(letter);
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
+
