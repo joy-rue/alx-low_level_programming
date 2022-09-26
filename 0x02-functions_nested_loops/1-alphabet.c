f6f7af1a69dc8e710233c5d0519e455f643380b1
joy-rue <ruejoysithole@gmail.com>
2022-09-26 05:33:36 +0000

    functions outside main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet- prints all the letters fo the alphabet
+ *
+ * Return: Void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
