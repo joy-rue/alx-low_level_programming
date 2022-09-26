83ccd97566ca96cb945dcfca8b0139b49c31b97b
joy-rue <ruejoysithole@gmail.com>
2022-09-26 06:02:05 +0000

    check if letter is lower case using >= a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - checks if the given letter is lower case
  *
+ *  @c: takes letter char to be checked
  *  Return: 1 if true and 0 if the letter is not lowercase
  */
 int _islower(int c)
