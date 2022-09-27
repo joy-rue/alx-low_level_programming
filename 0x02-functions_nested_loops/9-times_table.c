0d8d987e2d3a4d889f71f02d323d17fdda58b46a
joy-rue <ruejoysithole@gmail.com>
2022-09-27 06:53:04 +0000

    times table of 9 digits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,39 @@
 void times_table(void)
 {
 	int factor;
+	int x;
 	int factor1;
+	int y;
+	int res;
 
 	for (factor = 0; factor <= 9; factor++)
 	{
-		for (factor1 = 0; factor1 <= 9; factor1++)
+		for (factor1 = 0; factor1 <= 9; factor1++;
 		{
-			_putchar(factor * factor1 + '0');
-			_putchar(' ');
-			_putchar(',');
+			res = factor * factor1;
+
+			if (res > 9)
+			{
+				x = res % 10;
+				y = (res - x) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(y + '0');
+				_putchar(x + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+				_putchar(res + '0');
+			}
 		}
 		_putchar('\n');
 	}
 }
+
